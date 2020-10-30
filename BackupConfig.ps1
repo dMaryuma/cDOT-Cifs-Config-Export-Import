2c3c74f7ead9f241521445a0c4fa40e1c0944475
dMaryuma <68241370+dMaryuma@users.noreply.github.com>
2020-10-30 13:06:51 +0200

    Add files via upload

---
+++ BackupConfig.ps1
@@ -0,0 +1,58 @@
+﻿Param([parameter(Mandatory = $true)] [alias("c")] $Cluster,
+      [parameter(Mandatory = $true)] [alias("cdr")] $ClusterDR,
+      [parameter(Mandatory = $true)] [alias("cu")] $ClusterUser,
+      [parameter(Mandatory = $true)] [alias("svm")] $Vserver,
+      [parameter(Mandatory = $true)] [alias("svmdr")] $VserverDR,
+      [parameter(Mandatory = $true)] [alias("cp")] $ClusterPassword,
+      [parameter(Mandatory = $true)] [alias("cdru")] $ClusterDRUser,
+      [parameter(Mandatory = $true)] [alias("cdrp")] $ClusterDRPassword)
+
+
+$Error.Clear()
+
+# You need to install the latest DataONTAP Powershell Toolkit. You can find it here: http://mysupport.netapp.com/NOW/download/tools/powershell_toolkit/
+Import-Module DataONTAP
+
+$ClusPasswd = ConvertTo-SecureString $ClusterPassword -AsPlainText -Force
+$ClusCred = New-Object -typename System.Management.Automation.PSCredential -ArgumentList $ClusterUser, $ClusPasswd
+$ClusDRPasswd = ConvertTo-SecureString $ClusterDRPassword -AsPlainText -Force
+$ClusDRCred = New-Object -typename System.Management.Automation.PSCredential -ArgumentList $ClusterDRUser, $ClusDRPasswd
+try{
+    $Nc = Connect-NcController $Cluster -Credential $ClusCred -ErrorAction Stop 
+    $NcDR = Connect-NcController $ClusterDR -Credential $ClusDRCred -ErrorAction Stop
+}
+catch{
+    Write-Host "unable to connect to cluster, check your cred"
+    exit(1)
+}
+
+#get protected volumes
+$protectedVols = $NcDR | Get-NcSnapmirror -VserverContext $vserverDR |select SourceVolume 
+$vols = @()
+$vols += foreach($vol in $protectedVols) {get-ncvol -name $vol.SourceVolume -Controller $Nc -Vserver $Vserver}
+
+
+#get cifs share from protectedVols
+$cifsShares = foreach ($vol in $protectedVols) {Get-NcCifsShare -Controller $Nc -VserverContext $Vserver| ?{$_.Path -like "*$($vol.SourceVolume)*"}}
+
+#get ACL from protectedVols
+$cifsSharesACL = $cifsShares | Get-NcCifsShareAcl
+
+#get unix user
+$unixUsers = Get-NcNameMappingUnixUser -VserverContext $Vserver -Controller $Nc
+
+#get unix group
+$unixGroups = Get-NcNameMappingUnixGroup -VserverContext $Vserver -Controller $Nc
+
+#get name mapping 
+$nameMapping = Get-NcNameMapping -VserverContext $Vserver -Controller $Nc
+
+#export to xml
+$cifsShares | Export-Clixml -Path c:\Cifs.xml
+$cifsSharesACL | Export-Clixml -Path c:\CifsACL.xml
+$unixUsers | Export-Clixml -Path c:\unixUsers.xml
+$unixGroups | Export-Clixml -Path c:\unixGroups.xml
+$nameMapping | Export-Clixml -Path c:\nameMapping.xml
+$vols | Export-Clixml -Path c:\vols.xml
+
+if($Error.count -gt 0){Write-Host $Error[0]}else{Write-Host "Backup successfully $(get-date)" -ForegroundColor Green}
---
+++ ImportConfig.ps1
@@ -0,0 +1,103 @@
+﻿Param([parameter(Mandatory = $true)] [alias("c")] $Cluster,
+      [parameter(Mandatory = $true)] [alias("cdr")] $ClusterDR,
+      [parameter(Mandatory = $true)] [alias("cu")] $ClusterUser,
+      [parameter(Mandatory = $true)] [alias("svm")] $Vserver,
+      [parameter(Mandatory = $true)] [alias("svmdr")] $VserverDR,
+      [parameter(Mandatory = $true)] [alias("cp")] $ClusterPassword,
+      [parameter(Mandatory = $true)] [alias("cdru")] $ClusterDRUser,
+      [parameter(Mandatory = $true)] [alias("cdrp")] $ClusterDRPassword)
+
+
+$Error.Clear()
+
+# You need to install the latest DataONTAP Powershell Toolkit. You can find it here: http://mysupport.netapp.com/NOW/download/tools/powershell_toolkit/
+Import-Module DataONTAP
+
+$ClusPasswd = ConvertTo-SecureString $ClusterPassword -AsPlainText -Force
+$ClusCred = New-Object -typename System.Management.Automation.PSCredential -ArgumentList $ClusterUser, $ClusPasswd
+$ClusDRPasswd = ConvertTo-SecureString $ClusterDRPassword -AsPlainText -Force
+$ClusDRCred = New-Object -typename System.Management.Automation.PSCredential -ArgumentList $ClusterDRUser, $ClusDRPasswd
+try{
+    $Nc = Connect-NcController $Cluster -Credential $ClusCred -ErrorAction Stop 
+    $NcDR = Connect-NcController $ClusterDR -Credential $ClusDRCred -ErrorAction Stop
+}
+catch{
+    Write-Host "unable to connect to cluster, check your cred"
+    exit(1)
+}
+
+#export to xml
+$cifsShares = Import-Clixml -Path c:\Cifs.xml
+$cifsSharesACL = Import-Clixml -Path c:\CifsACL.xml
+$unixUsers = Import-Clixml -Path c:\unixUsers.xml
+$unixGroups = Import-Clixml -Path c:\unixGroups.xml
+$nameMapping = Import-Clixml -Path c:\nameMapping.xml
+$vols = Import-Clixml -Path c:\vols.xml
+
+
+#get protected volumes
+$volsDR = foreach ($vol in $vols) {Get-NcVol -Controller $NcDR -Name $vol.Name -VserverContext $VserverDR}
+
+#mount volsDR
+foreach ($volDR in $volsDR){
+    foreach ($vol in $vols){
+        if ($vol.Name -eq $volDR.Name){
+            Mount-NcVol -Name $volDR.Name -JunctionPath $vol.JunctionPath -VserverContext $VserverDR
+        }
+    }
+} 
+
+
+#create cifs share
+foreach ($volDR in $volsDR){
+    foreach ($share in $cifsShares){
+        if ($share.Volume -eq $volDR.Name){
+            try{
+            Add-NcCifsShare -Name $share.ShareName -Path $share.Path -ShareProperties $share.ShareProperties -SymlinkProperties $share.SymlinkProperties -VscanProfile $share.VscanFileopProfile -VserverContext $VserverDR -ErrorAction stop
+            }catch {Write-Warning "$($Error[0]) in $($share.Volume) for create cifs"}
+        }
+    }
+} 
+
+#modify cifs share acl
+foreach ($share in $cifsShares){
+    foreach ($acl in $cifsSharesACL){
+        if ($share.ShareName -eq $acl.Share){
+            try{
+            Add-NcCifsShareAcl -Share $acl.Share -UserOrGroup $acl.UserOrGroup -UserGroupType $acl.UserGroupType -VserverContext $VserverDR -Permission $acl.permission -Controller $NcDR -ErrorAction stop
+            }catch {Write-Warning "$($Error[0]) in $($share.ShareName) for create cifs ACL"}
+        }
+    }
+} 
+
+#create unix group
+foreach ($usr in $unixGroups){
+    try{
+    $usr | New-NcNameMappingUnixGroup -VserverContext $VserverDR -ErrorAction stop
+    }
+    catch{
+        Write-Warning "$($Error[0]) in $($usr.UserName) for create unix group"
+    }
+}
+
+#create unix users
+foreach ($usr in $unixUsers){
+    try{
+    $usr | New-NcNameMappingUnixUser -VserverContext $VserverDR -ErrorAction stop
+    }
+    catch{
+        Write-Warning "$($Error[0]) in $($usr.UserName) for create unix user"
+    }
+}
+
+#create name-mapping
+foreach ($name in $nameMapping){
+    try{
+    $name | New-NcNameMapping -VserverContext $VserverDR -ErrorAction stop
+    }
+    catch{
+        Write-Warning "$($Error[0]) in $($usr.UserName) for creating name-mapping"
+    }
+}
+
+if($Error.count -gt 0){Write-Host $Error[0]}else{Write-Host "import successfully $(get-date)" -ForegroundColor Green}
